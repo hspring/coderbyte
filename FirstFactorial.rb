485c2b01db0a71d853339b76d5424ec691fe7f72
hspring <sbkim0203@gmail.com>
2015-07-28 18:27:25 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,13 @@
+def FirstFactorial(num)
+ num.to_i
+  if num == 0
+    return 1
+  else 
+    return num*FirstFactorial(num-1)
+  end
+end 
+         
+
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)           
